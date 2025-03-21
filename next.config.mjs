4bdcd7172ab95bef759531fc086fc2a421b5d1e1
Alexsandroj <ajs6@cin.ufpe.br>
2025-03-21 17:22:15 -0300

    Permitir Todas as Origens no allowedDevOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    experimental: {
+      allowedDevOrigins: ['*'], // Permite todas as origens
+    },
+  };
 export default nextConfig;
